d4081950988ba984772425d16836ee40440354d1
Sentaro <ui-10@outlook.jp>
2023-09-26 17:18:14 +0900

    ewrwre

--- src/routes/Camera/Pickup.jsx
+++ src/routes/Camera/Pickup.jsx
@@ -15,7 +15,14 @@ function Pickup() {
   //canvas要素の大きさを変更
   canvas.width = cameraWidth;
   canvas.height = cameraHeight;
-  confirm()
+  ctx.clearRect(0, 0, 600, 600);
+  canvas.classList.add("hidden");
+  canvas.classList.toggle('fixed');
+  canvas.classList.toggle('z-[90]');
+  canvas.classList.toggle('left-1/2');
+  canvas.classList.toggle('top-1/2');
+  canvas.classList.toggle('translate-x-[-50%]');
+  canvas.classList.toggle('translate-y-[-50%]');
   //描画用オブジェクトを取得
   const ctx = canvas.getContext("2d");
 
--- src/routes/Camera/Retry.jsx
+++ src/routes/Camera/Retry.jsx
@@ -1,7 +1,6 @@
+
 function Retry() {
 
-
-  function retry() {
 
     const canvas = document.getElementById("canvas");
     //描画用オブジェクトを取得
@@ -9,19 +8,12 @@ function Retry() {
     
     ctx.clearRect(0, 0, 600, 600);
     canvas.classList.add("hidden");
-    confirm()
-    }
-
-
-    function confirm() {
-    const canvas = document.getElementById("canvas");
-    canvas.classList.toggle('fixed');
-    canvas.classList.toggle('z-[90]');
-    canvas.classList.toggle('left-1/2');
-    canvas.classList.toggle('top-1/2');
-    canvas.classList.toggle('translate-x-[-50%]');
-    canvas.classList.toggle('translate-y-[-50%]');
-  }
+      canvas.classList.toggle('fixed');
+      canvas.classList.toggle('z-[90]');
+      canvas.classList.toggle('left-1/2');
+      canvas.classList.toggle('top-1/2');
+      canvas.classList.toggle('translate-x-[-50%]');
+      canvas.classList.toggle('translate-y-[-50%]');
 
   return (
     <>
