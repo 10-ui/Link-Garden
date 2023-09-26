74509a77424188d066d8556f76aa9c67cee4dace
Sentaro <ui-10@outlook.jp>
2023-09-26 19:30:49 +0900

    dsad

--- src/routes/Camera/Delete.jsx
+++ src/routes/Camera/Delete.jsx
@@ -1,4 +1,4 @@
-import Decide from "./Decide";
+
 
 function Delete() {
 
--- src/routes/Camera/Pickup.jsx
+++ src/routes/Camera/Pickup.jsx
@@ -18,12 +18,12 @@ function Pickup() {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, 600, 600);
   canvas.classList.add("hidden");
-  canvas.classList.toggle('fixed');
-  canvas.classList.toggle('z-[90]');
-  canvas.classList.toggle('left-1/2');
-  canvas.classList.toggle('top-1/2');
-  canvas.classList.toggle('translate-x-[-50%]');
-  canvas.classList.toggle('translate-y-[-50%]');
+  canvas.classList.add('fixed');
+  canvas.classList.add('z-[90]');
+  canvas.classList.add('left-1/2');
+  canvas.classList.add('top-1/2');
+  canvas.classList.add('translate-x-[-50%]');
+  canvas.classList.add('translate-y-[-50%]');
   //描画用オブジェクトを取得
 
 
