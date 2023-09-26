8afad16342eb79dacec332684f61375122ce3507
Sentaro <ui-10@outlook.jp>
2023-09-26 19:33:11 +0900

    cxvxv

--- src/routes/Camera/Decide.jsx
+++ src/routes/Camera/Decide.jsx
@@ -40,8 +40,8 @@ function Decide() {
     const animal = document.getElementById('animals');
     animal.setAttribute('src',dataURI);
 
-    // // canvas の削除
-    // ctx.clearRect(0, 0, cameraWidth, cameraHeight);
+    // canvas の削除
+    ctx.clearRect(0, 0, cameraWidth, cameraHeight);
     canvas.classList.add("hidden");
     UI.classList.remove("hidden");
   }
