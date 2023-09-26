a0568d27be718dc51623004846718adf4c7e1556
Sentaro <ui-10@outlook.jp>
2023-09-26 18:07:10 +0900

    gdssd

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,8 +5,8 @@ function Home() {
 
   return (
     <>
-      <div className='h-screen w-[100%] mx-[37px] grid items-center'>
-        <div className='container'>
+      <div className='h-screen w-auto mx-[37px] grid items-center'>
+        <div>
           <img className='mx-auto mb-[70px]' src={logo} alt="ロゴアイコン" />
           <h1 className='mb-[24px] text-[24px] font-main font-bold text-center'>Link&&Gardenへようこそ！</h1>
           <p className='h-[71px] mb-[89px] text-[16px] text-center font-main font-bold'>動物たちの幸せを園内で集めてきたアイ<br/>テムや環境を通して学ぶことができます。</p>
