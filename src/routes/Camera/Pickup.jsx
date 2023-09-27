810157394b1fc1c2a3cba72aa5012a23193384d0
Sentaro <ui-10@outlook.jp>
2023-09-27 18:24:13 +0900

    fd

--- src/routes/Camera/Pickup.jsx
+++ src/routes/Camera/Pickup.jsx
@@ -186,7 +186,7 @@ function Pickup() {
 
   return (
     <>
-      <img  className='w-[40px] h-[40px] fixed left-1/2 top-[90%] translate-x-[-50%]' src={shatter} alt="撮影ボタン" onClick={() => canvas_input() }/>
+      <img  className='w-[70px] h-[70px] fixed left-1/2 bottom-[-120%] translate-x-[-50%]' src={shatter} alt="撮影ボタン" onClick={() => canvas_input() }/>
     </>
   )
 }
