7a77d2175e573b5bb62cd280d68e43dcee7a43d4
Sentaro <ui-10@outlook.jp>
2023-09-28 20:41:28 +0900

    小変更

--- src/routes/Camera/UI.jsx
+++ src/routes/Camera/UI.jsx
@@ -139,10 +139,10 @@ function UI() {
         <img className='mirror w-screen fixed left-1/2 top-[20%] translate-x-[-50%] hidden' src={Flamirror} alt="フラミンゴがたくさん映る鏡" />
         <div className="comment w-4/5 fixed left-1/2 top-[70%] translate-x-[-50%] hidden">
           <p>わたしはたくさんの<ruby>友達<rp>(</rp><rt>ともだち</rt><rp>)</rp> </ruby>といるのが
-          <ruby>好き<rp>(</rp><rt>すき</rt><rp>)</rp> </ruby>なの。
+          <ruby>好き<rp>(</rp><rt>すき</rt><rp>)</rp> </ruby>な<br/>の。
           <ruby>自分<rp>(</rp><rt>じぶん</rt><rp>)</rp> </ruby>の<ruby>姿<rp>(</rp><rt>すがた</rt><rp>)</rp> </ruby>が
           <ruby>写<rp>(</rp><rt>うつ</rt><rp>)</rp> </ruby>っていると
-          <ruby>友達<rp>(</rp><rt>ともだち</rt><rp>)</rp> </ruby>がいるようで
+          <ruby>友達<rp>(</rp><rt>ともだち</rt><rp>)</rp> </ruby>がいるよう<br/>で
           <ruby>安心<rp>(</rp><rt>あんしん</rt><rp>)</rp> </ruby>する！
           あなたも<ruby>友達<rp>(</rp><rt>ともだち</rt><rp>)</rp> </ruby>になってくれる？</p>
           <img src={Comment} alt="喜んでるときの吹き出し" />
