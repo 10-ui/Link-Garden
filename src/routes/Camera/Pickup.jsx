c7961c557af3a3b7ff6b79ea42c1441cef8e8bc8
Sentaro <ui-10@outlook.jp>
2023-09-29 19:01:10 +0900

    ss

--- src/routes/Camera/Pickup.jsx
+++ src/routes/Camera/Pickup.jsx
@@ -200,7 +200,7 @@ function Pickup() {
 
   return (
     <>
-      <div className='bg-screen backscreen h-screen w-screen z-[1] hidden'></div>
+      <div className='bg-screen relative backscreen h-screen w-screen z-[-2] hidden'></div>
       <img  className='hidden shoot w-[70px] h-[70px] fixed left-1/2 top-[89%] translate-x-[-50%]' src={shatter} alt="撮影ボタン" onClick={() => canvas_input() }/>
     </>
   )
