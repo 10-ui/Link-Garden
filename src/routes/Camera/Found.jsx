8573ad16d4f0f701241d0a22af7f0147410f73d0
Sentaro <ui-10@outlook.jp>
2023-09-29 20:22:27 +0900

    ko

--- src/routes/Camera/Found.jsx
+++ src/routes/Camera/Found.jsx
@@ -2,8 +2,8 @@ function Found(){
   return (
     <>
       <div className='bg-screen nice relative h-screen w-screen z-[0] hidden'>
-        <div className="mx-[30px]">
-          <h1 className="text-btntext text-[24px] text-left">フラミンゴと<ruby>鏡<rp>(</rp><rt>かがみ</rt><rp>)</rp></ruby></h1>
+        <div className="mx-[30px] mt-[60px]">
+          <h1 className="text-btntext mt-[40px] text-[24px] text-left">フラミンゴと<ruby>鏡<rp>(</rp><rt>かがみ</rt><rp>)</rp></ruby></h1>
           <p className="text-btntext"><ruby>自然界<rp>(</rp><rt>しぜんかい</rt><rp>)</rp></ruby>にいる
             フラミンゴは<ruby>数百<rp>(</rp><rt>すうひゃく</rt><rp>)</rp> </ruby>～<ruby>数千羽<rp>(</rp><rt>すうせんわ</rt><rp>)</rp></ruby>の
             <ruby>群<rp>(</rp><rt>む</rt><rp>)</rp></ruby>れをつくり、
@@ -14,14 +14,22 @@ function Found(){
             フラミンゴには<ruby>仲間<rp>(</rp><rt>なかま</rt><rp>)</rp> </ruby>が
             <ruby>少<rp>(</rp><rt>すく</rt><rp>)</rp></ruby>なくても<ruby>安心<rp>(</rp><rt>あんしん</rt><rp>)</rp></ruby>
             できるように、<ruby>壁<rp>(</rp><rt>かべ</rt><rp>)</rp></ruby>に<ruby>鏡<rp>(</rp><rt>かがみ</rt><rp>)</rp></ruby>を
-            <ruby>貼<rp>(</rp><rt>は</rt><rp>)</rp></ruby>り<ruby>付<rp>(</rp><rt>つ</rt><rp>)</rp></ruby>け
+            <ruby>貼<rp>(</rp><rt>は</rt><rp>)</rp></ruby>り<ruby>付<rp>(</rp><rt>つ</rt><rp>)</rp></ruby>け、
             <ruby>多<rp>(</rp><rt>おお</rt><rp>)</rp></ruby>く<ruby>見<rp>(</rp><rt>み</rt><rp>)</rp></ruby>せる
             <ruby>工夫<rp>(</rp><rt>くふう</rt><rp>)</rp></ruby>をしています。
             では、<ruby>実際<rp>(</rp><rt>じっさい</rt><rp>)</rp></ruby>に
             <ruby>鏡<rp>(</rp><rt>かがみ</rt><rp>)</rp></ruby>のある<ruby>環境<rp>(</rp><rt>かんきょう</rt><rp>)</rp></ruby>で
             <ruby>過<rp>(</rp><rt>す</rt><rp>)</rp></ruby>ごしている
             フラミンゴの<ruby>様子<rp>(</rp><rt>ようす</rt><rp>)</rp></ruby>を
-            <ruby>見<rp>(</rp><rt>み</rt><rp>)</rp></ruby>てみましょう！</p>
+            <ruby>見<rp>(</rp><rt>み</rt><rp>)</rp></ruby>てみましょう！
+          </p>
+          <iframe 
+            className="w-[80%] h-auto" 
+            src="https://www.youtube.com/embed/u2TRshJ51jg?si=DmwS5jLo-m2vlyZ8" 
+            title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowfullscreen>
+          </iframe>
         </div>
       </div>
 
