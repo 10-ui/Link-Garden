356e14ad0e8924b8bc1821029a838a50edeaab38
Sentaro <ui-10@outlook.jp>
2023-11-02 18:29:15 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Items from './routes/Get.jsx';
 import Result from './routes/Result.jsx';
 import Camera from './routes/Camera.jsx';
 import Cushion from './routes/Cushion.jsx';
+import House from './routes/House.jsx'
+import Found from "./routes/Camera/Found.jsx";
 
 
 function App() {
@@ -13,11 +15,13 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={ < Home /> } />
+        <Route path="/house" element={< House />}/>
         <Route path="/Bluetooth" element={ < Bluetooth /> } />
         <Route path="/Getitem" element={ < Items /> } />
         <Route path="/Result" element={ < Result /> } />
         <Route path="/Nuriecamera" element={ <Camera /> } />
         <Route path="/Cushion" element={ <Cushion /> } />
+        <Route path="/Found" element={ <Found /> } />
       </Routes>
     </>
   )
---
+++ src/routes/House.jsx
@@ -0,0 +1,23 @@
+import link from '../assets/icon/mark_link.svg';
+import pn from '../assets/icon/pagenation_2.svg';
+import { Link } from "react-router-dom";
+
+function House() {
+
+  return (
+    <>
+      <div className='h-screen w-auto mx-[20px] grid items-center'>
+        <div className='w-[100%] mx-auto'>
+          <img className='w-[97%] mx-auto mb-[58px]' src={link} alt="アイテム送受信" />
+          <h1 className='text-[24px] font-bold text-center font-main mb-[24px]'>集めてきたアイテムを<br/>スマートフォンに移そう！</h1>
+          <p className='h-[69px] font-bold text-[16px] text-center mb-[56px] font-main'>スタンプをスマートフォンの近くに置き、<br/>アイテムの取得開始ボタンを押してください</p>
+          <img className='block mx-auto mb-[24px]' src={pn} alt="pagenation" />
+          <Link to='/Found' className='text-[20px] w-[97%] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded'>アイテムの取得開始</Link>
+          {/* onClick={()=> Connect()} */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default House
