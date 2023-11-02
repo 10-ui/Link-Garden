9ac14b0f5387dfef7bb39bd7be98a8ac19ecc513
Sentaro <ui-10@outlook.jp>
2023-11-02 18:50:58 +0900

    Homes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
           <h1 className='mb-[24px] text-[24px] font-main font-bold text-center'>Link&&Gardenへようこそ！</h1>
           <p className='h-[71px] mb-[46px] text-[16px] text-center font-main font-bold'>動物たちの幸せを園内で集めてきたアイ<br/>テムや環境を通して学ぶことができます。</p>
           <Link to='/Bluetooth' className='text-[20px] text-white w-[100%] mb-[12px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded'>イベントで遊ぶ</Link>
-          <Link to='/House' className='text-[20px] text-white w-[100%] mb-[12px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded'>おうちで学ぶ</Link>
+          <Link to='/House' className='text-[20px] text-white w-[100%] mb-[26px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded'>おうちで学ぶ</Link>
         </div>
       </div>
     </>
