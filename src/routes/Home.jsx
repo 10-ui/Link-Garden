cfa2677cc47c89cfb08f3039e8f6ee036c05fe13
Sentaro <ui-10@outlook.jp>
2023-11-04 12:23:32 +0900

    logoボタン

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,43 @@
-import logo from '../assets/icon/mark_logo.svg';
+import logo from "../assets/icon/mark_logo.svg";
 import { Link } from "react-router-dom";
 
 function Home() {
 
+  function hide() {
+    document.querySelector('.hides').classList.add('hidden');
+  }
+
   return (
     <>
-      <div className='h-screen  mx-[25px] grid items-center box-border'>
-        <div className='w-[100%] mx-auto'>
-          <img className='mx-auto  mb-[70px]' src={logo} alt="ロゴアイコン" />
-          <h1 className='mb-[24px] text-[24px] font-main font-bold text-center'>Link&&Gardenへようこそ！</h1>
-          <p className='h-[71px] mb-[46px] text-[16px] text-center font-main font-bold'>動物たちの幸せを園内で集めてきたアイ<br/>テムや環境を通して学ぶことができます。</p>
-          <Link to='/Bluetooth' className='text-[20px] text-white w-[100%] mb-[12px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded'>イベントで遊ぶ</Link>
-          <Link to='/House' className='text-[20px] text-white w-[100%] mb-[26px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded'>おうちで学ぶ</Link>
+      <div className="h-screen  mx-[25px] grid items-center box-border">
+        <div className="w-[100%] mx-auto">
+          <img className="mx-auto  mb-[70px]" src={logo} alt="ロゴアイコン" />
+          <h1 className="mb-[24px] text-[24px] font-main font-bold text-center">
+            Link&&Gardenへようこそ！
+          </h1>
+          <p className="h-[71px] mb-[46px] text-[16px] text-center font-main font-bold">
+            動物たちの幸せを園内で集めてきたアイ
+            <br />
+            テムや環境を通して学ぶことができます。
+          </p>
+          <Link
+            to="/Bluetooth"
+            className="text-[20px] text-white w-[100%] mb-[12px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded"
+            onClick={() => hide()}
+          >
+            イベントで遊ぶ
+          </Link>
+          <Link
+            to="/House"
+            className="text-[20px] text-white w-[100%] mb-[26px] block mx-auto text-center py-[13px] bg-accent font-main font-bold rounded"
+            onClick={() => hide()}
+          >
+            おうちで学ぶ
+          </Link>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/routes/House.jsx
+++ src/routes/House.jsx
@@ -1,55 +1,62 @@
-import rope from '../assets/icon/item_rope.svg';
-import cardboard from '../assets/icon/item_cardboard.svg';
-import mirror from '../assets/icon/item_mirror.svg';
-import branch from '../assets/icon/item_branch.svg';   
-import bag from '../assets/icon/item_bag.svg';
-import no_rope from '../assets/icon/item_no_rope.svg';
-import no_cardboard from '../assets/icon/item_no_cardboard.svg';
-import no_mirror from '../assets/icon/item_no_mirror.svg';
-import no_branch from '../assets/icon/item_no_branch.svg';   
-import no_bag from '../assets/icon/item_no_bag.svg';
-import pn from '../assets/icon/pagenation_3.svg';
+import rope from "../assets/icon/item_rope.svg";
+import cardboard from "../assets/icon/item_cardboard.svg";
+import mirror from "../assets/icon/item_mirror.svg";
+import branch from "../assets/icon/item_branch.svg";
+import bag from "../assets/icon/item_bag.svg";
+import no_rope from "../assets/icon/item_no_rope.svg";
+import no_cardboard from "../assets/icon/item_no_cardboard.svg";
+import no_mirror from "../assets/icon/item_no_mirror.svg";
+import no_branch from "../assets/icon/item_no_branch.svg";
+import no_bag from "../assets/icon/item_no_bag.svg";
+import logo from "../assets/icon/mark_logo.svg";
 import { Link } from "react-router-dom";
 
-
-
 function Result() {
-
-
   function no_use() {
-    {no_bag}
-    {no_branch}
-    {no_cardboard}
-    {no_mirror}
-    {no_rope}
+    {
+      no_bag;
+    }
+    {
+      no_branch;
+    }
+    {
+      no_cardboard;
+    }
+    {
+      no_mirror;
+    }
+    {
+      no_rope;
+    }
   }
 
+  let num = 5;
 
-  let num = 5
-
-
-  return(
+  return (
     <>
-      <div className='w-auto'>
+      <div className="w-auto">
         <div className="h-screen w-auto  mx-[20px] grid items-center">
-          <div className='w-[100%] mx-auto'>
-            <h1 className=" text-white font-main font-bold text-[20px] text-center mb-[48px]">アイテムを選んでね！</h1>
-            <p className='text-[12px] text-center font-main font-bold mb-[45px]'>アイテムで幸せになれる動物がいるよ！</p>
-            <div className="wrapper grid grid-cols-3 items-center justify-center w-fit gap-4 mx-auto mb-[55px]">
+          <div className="w-[100%] mx-auto">
+            <h1 className=" text-white font-main font-bold text-[20px] text-center mb-[48px]">
+              アイテムを選んでね！
+            </h1>
+            <p className="text-[12px] text-center font-main font-bold mb-[45px]">
+              アイテムで幸せになれる動物がいるよ！
+            </p> 
+            <div className="scale-95 wrapper grid grid-cols-3 items-center justify-center w-fit gap-4 mx-auto mb-[55px]">
               <img src={rope} alt="縄" />
               <img src={cardboard} alt="ダンボール" />
-              <Link to="/Found"><img src={mirror} alt="鏡" /></Link>
+              <Link to="/Found">
+                <img src={mirror} alt="鏡" />
+              </Link>
               <img src={branch} alt="木の枝" />
-              <img src={bag} alt="袋" /> 
+              <img src={bag} alt="袋" />
             </div>
-            {/* <p className='text-[18px] font-main font-bold mb-[55px]'>さっそくアイテムを使ってどうぶつが好きなアイテムを見つけに行こう！</p> */}
-            {/* <img className='block mx-auto mb-[24px]' src={pn} alt="ページ３" /> */}
-            {/* <Link to='/Nuriecamera' className='text-[20px] w-[100%] mx-auto block bg-accent text-center py-[13px] font-main font-bold rounded'>Link&&Gardenの世界に入る</Link> */}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Result
+export default Result;
